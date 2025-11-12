deb6e4bf30d6ef421a5321d8b391ac8cc10d7d25
parkajeong <milk5253@gmail.com>
2025-11-12 18:09:01 +0900

    [Fix] set() 내 l_m 감소 로직 수정

--- project02-diablo/Diablo-m-test.c
+++ project02-diablo/Diablo-m-test.c
@@ -909,7 +909,7 @@ void set()
             getch();
             user.exp -= monster.exp;
             user.gold += monster.gold;
-            l_m--;
+            if(l_m>0){l_m--};
             if (user.exp <= 0)
             {
                 clrscr();
